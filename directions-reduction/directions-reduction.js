380ae59fc7605c308d822bf390477395aba302cb
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-10-10 15:18:33 +0300

    directions-reduction

---
+++ directions-reduction/directions-reduction.js
@@ -0,0 +1,27 @@
+function dirReduc(arr) {
+  const opposites = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+
+  const result = [];
+
+  arr.forEach((item) => {
+    if (result.length) {
+      const prevItem = result.pop();
+
+      if (prevItem !== opposites[item]) {
+        result.push(prevItem);
+        result.push(item);
+      }
+    } else {
+      result.push(item);
+    }
+  });
+
+  return result;
+}
+
+console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]));
