343f253673eed6a537d29c811f3c2b29c7150677
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-10-28 23:55:37 +0300

    new info

--- data-reverse/data-reverse.js
+++ data-reverse/data-reverse.js
@@ -12,3 +12,9 @@ console.log(
     0, 1, 0, 1, 0, 1, 0,
   ])
 );
+
+// const bytes = [];
+// for (let i = 0; i < data.length; i += 8) {
+//   bytes.unshift(...data.slice(i, i + 8));
+// }
+// return bytes;
