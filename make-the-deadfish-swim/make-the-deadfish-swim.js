47e7690e341590f5437168a4ec9f740f98c69ee2
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-10-29 23:32:23 +0300

    make-the-deadfish-swim

---
+++ make-the-deadfish-swim/make-the-deadfish-swim.js
@@ -0,0 +1,21 @@
+// Return the output array, and ignore all non-op characters
+function parse(data) {
+  var response = [];
+  var num = 0;
+
+  data.split("").forEach(function (val) {
+    if (val == "d") {
+      num--;
+    } else if (val == "i") {
+      num++;
+    } else if (val == "s") {
+      num = num * num;
+    } else if (val == "o") {
+      response.push(num);
+    }
+  });
+
+  return response;
+}
+
+console.log(parse("iiisxxxdoso"));
