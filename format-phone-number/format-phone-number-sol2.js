e81537c01f42d6721bb730bd14b67cfd8fa02899
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-11-21 23:24:29 +0300

    format-phone-number

---
+++ format-phone-number/format-phone-number-sol2.js
@@ -0,0 +1,7 @@
+function formatPhoneNumber(numbers) {
+  const formatted = numbers.join("");
+  return `(${formatted.substring(0, 3)}) ${formatted.substring(
+    3,
+    6
+  )}-${formatted.substring(6)}`;
+}
