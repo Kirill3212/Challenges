c7c1513fec07a3aba0b8d4bd5cc788a6f80040e4
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-11-23 22:19:39 +0300

    are-all-chars-unique

---
+++ are-all-chars-unique/are-all-chars-unique-sol2.js
@@ -0,0 +1,13 @@
+function areAllCharactersUnique(str) {
+  const charCount = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char]) {
+      return false;
+    }
+    charCount[char] = true;
+  }
+
+  return true;
+}
